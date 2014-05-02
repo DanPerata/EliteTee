580ce75a83a340e6de55935ecda0fafddefaaf16
Dan Perata <djperata@aol.com>
2014-05-01 18:24:19 -0600

    Added a random number generator to calculate yardage based on how closely ball is stopped to the pin.  Added save function to allow data to be grouped by session. Created results div to append to and display stats.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(document).on('ready', function() {
   
 });
+
+// var scoreString = session.score.join(', ');
+// var sessionScore = $('<h3>' + scoreString + '</h3>');
+// $('#scoreDiv').append(sessionScore);
+
+
